38f4d6ce448ba425d0d0eb70c6af8feccc2875cd
sfwani <code.sanaan@gmail.com>
2025-05-04 17:57:40 -0400

    vercel error codes

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,6 +1,8 @@
 import { defineCollection, z } from 'astro:content';
+import { glob } from 'astro/loaders';
 
 const writeups = defineCollection({
+  loader: glob({ pattern: '**/*.md', base: 'src/content/writeups' }),
   schema: z.object({
     title: z.string(),
     date: z.date(),
